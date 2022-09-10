1160ed6fdcbf783755c5d558be54d450b7250a70
Izzat Madaminov <izzat.madaminov@relentless.agency>
2022-09-10 19:27:59 +0500

    Final Task

--- src/main/java/lang/print/gaps/finalModuleTask/NumbersPrinter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumbersPrinter.java
@@ -2,6 +2,7 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumbersPrinter {
     public void printIsPositive(int number) {
-
+        String s = Integer.toBinaryString(number);
+        System.out.println(number > 0);
     }
 }
